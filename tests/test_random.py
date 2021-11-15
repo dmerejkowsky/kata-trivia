cbd98d58b773bbfb70d60a2a8bfbccd294e0979a
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-11-15 15:22:15 +0100

    rename test file

---
+++ tests/test_random.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+import pytest
+
+from conftest import FakeRandomSource
+from trivia import RandomSource
+
+
+def test_using_a_fake_random_as_iterator():
+    """Given start, stop and a sequence of results,
+    make sure that call to random_in_range() returns
+    the proper values
+    """
+
+    random_source = FakeRandomSource(start=1, end=6)
+    random_source.set_reference([6, 1, 3])
+
+    actual = list(random_source)
+    assert actual == [6, 1, 3]
+
+
+def test_fake_random_checks_that_references_are_in_range():
+    random_source = FakeRandomSource(start=1, end=6)
+    with pytest.raises(ValueError):
+        random_source.set_reference([6, 7, 3])
+
+
+def test_fake_random_using_reference():
+    path = Path("reference/randomSeq.txt")
+    lines = path.read_text().splitlines()
+    random_source = FakeRandomSource.from_reference(lines[0:2])
+    assert random_source.start == 0
+    assert random_source.end == 9
+    first_three = [next(random_source) for i in range(0, 3)]
+    assert first_three == [6, 9, 2]
+
+
+def test_random_source():
+    random_source = RandomSource(1, 4)
+    actual = next(random_source)
+    assert 1 <= actual < 4
