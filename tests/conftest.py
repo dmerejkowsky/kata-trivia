9c1606e33964ff7364ab0fe4790134dbb34aedcd
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-11-15 14:57:34 +0100

    More passing tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,9 +6,21 @@ class FakeRandomSource:
         self._index = 0
 
     def set_reference(self, numbers):
+        for i, number in enumerate(numbers):
+            if number < self.start or number > self.end:
+                raise ValueError(
+                    f"Value {number} at index {i} is not in expected range"
+                )
+
         self._reference = numbers
 
-    def random_in_range(self):
-        res = self._reference[self._index]
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            res = self._reference[self._index]
+        except IndexError:
+            raise StopIteration
         self._index += 1
         return res
--- tests/test_fake_random.py
+++ tests/test_fake_random.py
@@ -1,7 +1,8 @@
 from conftest import FakeRandomSource
+import pytest
 
 
-def test_fake_random():
+def test_using_a_fake_random_as_iterator():
     """Given start, stop and a sequence of results,
     make sure that call to random_in_range() returns
     the proper values
@@ -10,5 +11,19 @@ def test_fake_random():
     random_source = FakeRandomSource(start=1, end=6)
     random_source.set_reference([6, 1, 3])
 
-    actual = [random_source.random_in_range() for _ in range(0, 3)]
+    actual = list(random_source)
     assert actual == [6, 1, 3]
+
+
+def test_check_that_references_are_in_range():
+    random_source = FakeRandomSource(start=1, end=6)
+    with pytest.raises(ValueError):
+        random_source.set_reference([6, 7, 3])
+
+
+def test_raises_stop_iteration():
+    random_source = FakeRandomSource(start=1, end=6)
+    random_source.set_reference([6, 1])
+
+    as_list = list(random_source)
+    assert as_list == [6, 1]
