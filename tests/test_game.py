7098042f5a32de72afcedbcfd7a1cf4a1a1dd3ef
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-11-16 15:21:41 +0100

    Rewrite game tests

--- tests/test_game.py
+++ tests/test_game.py
@@ -44,31 +44,10 @@ def test_wraps_next_player(game):
     assert game.current_player.name == "Alice"
 
 
-def test_is_sent_to_penalty_box_on_wrong_answer(game):
-    game.add("Alice")
-
-    game.wrong_answer()
-
-    assert game.current_player.in_penalty_box
-
-
-def test_answering_correctly_when_getting_out_of_the_penalty_box(game):
-    alice = game.add("Alice")
-    alice.in_penalty_box = True
-    alice.is_getting_out_of_penalty_box = True
-    alice.purse = 3
-
-    game.correct_answer()
-
-    assert alice.purse == 4
-    assert not game.has_ended
-
-
 def test_answering_correctly_and_winning_the_game(game):
     game.add("Alice")
     game.current_player.purse = 5
 
     game.correct_answer()
 
-    assert game.current_player.purse == 6
     assert game.has_ended
